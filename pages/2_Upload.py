6e77f643aaca9d9f8a6abc7a7d086eae7afbc831
scott-dunphy <133469585+scott-dunphy@users.noreply.github.com>
2024-07-06 18:30:34 -0400

    Create 2_Upload.py

---
+++ pages/2_Upload.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+from loan import Loan
+from property import Property
+import upload
+
+st.title('Property and Loan Importer')
+    
+properties_and_loans_file = st.file_uploader('Upload Properties and Loans Excel File', type=['xlsx'])
+cashflows_file = st.file_uploader('Upload Cashflows Excel File', type=['xlsx'])
+
+if properties_and_loans_file and cashflows_file:
+    properties = load_properties_and_loans(properties_and_loans_file)
+    noi, capex = load_cashflows(cashflows_file)
+    
+    for property_obj in properties:
+        property_obj.noi = noi.get(property_obj.property_id, {})
