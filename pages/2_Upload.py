546e6c71524645205a5f7defcbc5558aa2911cd4
scott-dunphy <133469585+scott-dunphy@users.noreply.github.com>
2024-07-06 19:44:03 -0400

    Update 2_Upload.py

--- pages/2_Upload.py
+++ pages/2_Upload.py
@@ -7,12 +7,12 @@ from upload import load_cashflows, load_properties_and_loans
 st.title('Property and Loan Importer')
     
 properties_and_loans_file = st.file_uploader('Upload Properties and Loans Excel File', type=['xlsx'])
-cashflows_file = st.file_uploader('Upload Cashflows Excel File', type=['xlsx'])
+#cashflows_file = st.file_uploader('Upload Cashflows Excel File', type=['xlsx'])
 
 if st.button("Upload Portfolio"):
     if properties_and_loans_file and cashflows_file:
         properties, loans = load_properties_and_loans(properties_and_loans_file)
-        noi, capex = load_cashflows(cashflows_file)
+        noi, capex = load_cashflows(properties_and_loans_file)
         
         for property_obj in properties:
             property_obj.noi = noi.get(property_obj.property_id, {})
