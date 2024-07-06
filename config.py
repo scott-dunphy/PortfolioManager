6145e921bead2164291585a0fee6c0d78a0be8c8
scott-dunphy <133469585+scott-dunphy@users.noreply.github.com>
2024-07-06 16:19:14 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+adjusted_column_config = {
+    'Adjusted Purchase Price': st.column_config.NumberColumn("Adjusted Purchase Price", format='%d'),
+    'Adjusted Loan Proceeds': st.column_config.NumberColumn("Adjusted Loan Proceeds", format='%d'),
+    'Adjusted Net Operating Income': st.column_config.NumberColumn("Adjusted Net Operating Income", format='%d'),
+    'Adjusted Capital Expenditures': st.column_config.NumberColumn("Adjusted Capital Expenditures", format='%d'),
+    'Adjusted Interest Expense': st.column_config.NumberColumn("Adjusted Interest Expense", format='%d'),
+    'Adjusted Principal Payments': st.column_config.NumberColumn("Adjusted Principal Payments", format='%d'),
+    'Adjusted Debt Scheduled Repayment': st.column_config.NumberColumn("Adjusted Debt Scheduled Repayment", format='%d'),
+    'Adjusted Debt Early Prepayment': st.column_config.NumberColumn("Adjusted Debt Early Prepayment", format='%d'),
+    'Adjusted Sale Proceeds': st.column_config.NumberColumn("Adjusted Sale Proceeds", format='%d'),
+    'Adjusted Partner Buyout': st.column_config.NumberColumn("Adjusted Partner Buyout", format='%d'),
+    'Total Cash Flow': st.column_config.NumberColumn("Total Cash Flow", format='%d')
+}
