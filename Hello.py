22e1b165e136acf86cc7fbbdc373624772ce8e2b
scott-dunphy <133469585+scott-dunphy@users.noreply.github.com>
2024-07-06 10:08:42 -0400

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,11 +1,27 @@
 import streamlit as st
+from portfolio import Portfolio
+import pandas as pd
 
 st.set_page_config(
-    page_title="Hello",
+    page_title="CRE Portfolio Manager",
     page_icon="👋",
 )
 
 st.write("# Welcome to Streamlit! 👋")
 
 st.sidebar.success("Select a demo above.")
+
+properties = st.session_state.properties
+
+portfolio = Portfolio(
+    name = 'Dunphy Property Fund',
+    start_date = pd.Datetime(2023,1,1),
+    end_date = pd.Datetime(2025,1,1),
+    properties = properties
+)
+
+cash_flows = portfolio.aggregate_hold_period_cash_flows()
+
+st.table(cash_flows)
+
 
