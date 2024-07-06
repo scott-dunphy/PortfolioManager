e36d7d6c933675a6e0d3115fb8d9ef9269dd1950
scott-dunphy <133469585+scott-dunphy@users.noreply.github.com>
2024-07-06 16:18:40 -0400

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -62,22 +62,7 @@ if 'properties' in st.session_state:
     )
     
     cash_flows = portfolio.aggregate_hold_period_cash_flows()
-
-    
-    column_config = {
-        'Adjusted Purchase Price': st.column_config.NumberColumn("Adjusted Purchase Price", format='%d'),
-        'Adjusted Loan Proceeds': st.column_config.NumberColumn("Adjusted Loan Proceeds", format='%d'),
-        'Adjusted Net Operating Income': st.column_config.NumberColumn("Adjusted Net Operating Income", format='%d'),
-        'Adjusted Capital Expenditures': st.column_config.NumberColumn("Adjusted Capital Expenditures", format='%d'),
-        'Adjusted Interest Expense': st.column_config.NumberColumn("Adjusted Interest Expense", format='%d'),
-        'Adjusted Principal Payments': st.column_config.NumberColumn("Adjusted Principal Payments", format='%d'),
-        'Adjusted Debt Scheduled Repayment': st.column_config.NumberColumn("Adjusted Debt Scheduled Repayment", format='%d'),
-        'Adjusted Debt Early Prepayment': st.column_config.NumberColumn("Adjusted Debt Early Prepayment", format='%d'),
-        'Adjusted Sale Proceeds': st.column_config.NumberColumn("Adjusted Sale Proceeds", format='%d'),
-        'Adjusted Partner Buyout': st.column_config.NumberColumn("Adjusted Partner Buyout", format='%d'),
-        'Total Cash Flow': st.column_config.NumberColumn("Total Cash Flow", format='$%d')
-    }
     
     # Display the DataFrame with custom formatting
-    st.dataframe(cash_flows, column_config=column_config, use_container_width=True)
+    st.dataframe(cash_flows, column_config=adjusted_column_config, use_container_width=True)
 
