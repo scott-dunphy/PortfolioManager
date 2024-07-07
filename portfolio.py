8c7b196c9484bcc1538849256e4aea45135c95c7
scott-dunphy <133469585+scott-dunphy@users.noreply.github.com>
2024-07-07 15:50:35 -0400

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -58,7 +58,7 @@ class Portfolio:
       # Aggregate loan cash flows
       if self.unsecured_loans:
         for loan in self.unsecured_loans:
-            loan_cf = loan.calculate_unsecured_cash_flows()
+            loan_cf = loan.get_unsecured_schedule()
             # Ensure the DataFrame is within the specified date range
             loan_cf = loan_cf[(loan_cf.index >= self.start_date) & (loan_cf.index <= self.end_date)]
             aggregate_cf = aggregate_cf.add(loan_cf, fill_value=0)
