c794372b50bd76a4d890e30e5433ee5716b2e9f6
scott-dunphy <133469585+scott-dunphy@users.noreply.github.com>
2024-07-07 15:48:06 -0400

    Update 2_Upload.py

--- pages/2_Upload.py
+++ pages/2_Upload.py
@@ -20,5 +20,5 @@ if st.button("Upload Portfolio"):
             property_obj.noi = noi.get(property_obj.property_id, {})
     
     st.session_state.properties = properties
-    portfolio = Portfolio(name='XYZ', properties=properties, analysis_start_date=datetime(2023,1,1), analysis_end_date=datetime(2030,12,1)))
+    portfolio = Portfolio(name='XYZ', properties=properties, analysis_start_date=datetime(2023,1,1), analysis_end_date=datetime(2030,12,1))
     st.session_state.portfolio = portfolio
