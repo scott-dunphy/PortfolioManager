0eebcbf827a531b526516859a42c21f2008e7a08
scott-dunphy <133469585+scott-dunphy@users.noreply.github.com>
2024-07-07 10:20:55 -0400

    Update 3_Unsecured Loans.py

--- pages/3_Unsecured Loans.py
+++ pages/3_Unsecured Loans.py
@@ -19,6 +19,8 @@ origination_date = pd.to_datetime(st.date_input('Origination Date'))
 maturity_date = pd.to_datetime(st.date_input('Maturity Date'))
 original_balance = st.number_input('Original Balance')
 note_rate = st.number_input('Note Rate (%)', min_value=0.0)
+spread = st.number_input('Floating Rate Spread (%)', min_value=0.0)
+fixed_floating = st.selectbox('Fixed/Floating', options=["Fixed", "Floating"])
 interest_only_period = st.number_input('Interest Only Period (months)')
 amortization_period = st.number_input('Amortization Period (months)')
 day_count_method = st.selectbox('Day Count Method', options=["Actual/360", "Actual/365", "30/360"])
@@ -31,7 +33,9 @@ loan = Loan(
   interest_only_period = interest_only_period,
   amortization_period = amortization_period,
   day_count_method = day_count_method,
-  loan_id = 'U01'
+  loan_id = 'U01',
+  fixed_floating = fixed_floating,
+  spread = spread
 )
 
 st.dataframe(loan.get_unsecured_schedule())
