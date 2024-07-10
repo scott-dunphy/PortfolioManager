44cc846ed1745bcf3b20c348aab256a0a78dc3d0
scott-dunphy <133469585+scott-dunphy@users.noreply.github.com>
2024-07-09 21:26:00 -0400

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -53,7 +53,7 @@ class Portfolio:
         # Aggregate property cash flows
         for property in self.properties:
             property_cf = property.hold_period_cash_flows_x(start_date=self.start_date, end_date=self.end_date)
-            property_cf.index = property_cf.index.map(lambda x: x.date() if isinstance(x, (pd.Timestamp, datetime)) else x)  # Ensure index is in date format
+            property_cf.index = property_cf.index.map(lambda x: x)  # Ensure index is in date format
             # Ensure the DataFrame is within the specified date range
             property_cf = property_cf[(property_cf.index >= self.start_date) & (property_cf.index <= self.end_date)]
             aggregate_cf = aggregate_cf.add(property_cf, fill_value=0)
