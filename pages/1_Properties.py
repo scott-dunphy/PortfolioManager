c3c06b62931d7b549adb1e714d93b76b8284d111
scott-dunphy <133469585+scott-dunphy@users.noreply.github.com>
2024-07-13 10:39:39 -0400

    Update 1_Properties.py

--- pages/1_Properties.py
+++ pages/1_Properties.py
@@ -118,9 +118,8 @@ if 'properties' in st.session_state and st.session_state.properties:
 
         st.write(selected_property.noi_capex)
         # Financial Data inputs
-        #with st.expander("Financial Data"):
-            #noi_data = st.text_area("Net Operating Income (space-separated values)", value=' '.join([str(v) for v in selected_property.noi.values()]))
-            #capex_data = st.text_area("Capital Expenditures (space-separated values)", value=' '.join([str(v) for v in selected_property.capex.values()]))
+        with st.expander("Financial Data"):
+            fin_df = st.data_editor(selected_property.noi_capex)
 
         if st.button('Update and Recalculate'):
             updated_data = {
