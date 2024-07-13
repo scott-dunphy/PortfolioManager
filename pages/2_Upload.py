c4cc54c2dca08028ae414b577fdd7acc0d412398
scott-dunphy <133469585+scott-dunphy@users.noreply.github.com>
2024-07-12 21:24:00 -0400

    Update 2_Upload.py

--- pages/2_Upload.py
+++ pages/2_Upload.py
@@ -13,9 +13,7 @@ now = date.today()
 start_date = date(now.year, now.month, 1)
 end_date = date(start_date.year + 3, start_date.month, 1)
 
-    
 properties_and_loans_file = st.file_uploader('Upload Properties and Loans Excel File', type=['xlsx'])
-#cashflows_file = st.file_uploader('Upload Cashflows Excel File', type=['xlsx'])
 
 if st.button("Upload Portfolio"):
     if properties_and_loans_file:
@@ -23,8 +21,10 @@ if st.button("Upload Portfolio"):
         noi, capex = load_cashflows(properties_and_loans_file)
         
         for property_obj in properties:
-            property_obj.noi = noi.get(property_obj.property_id, {})
-    
-    st.session_state.properties = properties
-    portfolio = Portfolio(name='Dunphy', properties=properties, start_date=start_date, end_date=end_date)
-    st.session_state.portfolio = portfolio
+            property_id = property_obj.property_id
+            property_obj.noi = noi.get(property_id, {})
+            property_obj.capex = capex.get(property_id, {})
+        
+        st.session_state.properties = properties
+        portfolio = Portfolio(name='Dunphy', properties=properties, start_date=start_date, end_date=end_date)
+        st.session_state.portfolio = portfolio
