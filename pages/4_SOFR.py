e8814a383925174307068561982c2c658f5555e3
scott-dunphy <133469585+scott-dunphy@users.noreply.github.com>
2024-07-13 16:02:11 -0400

    Update 4_SOFR.py

--- pages/4_SOFR.py
+++ pages/4_SOFR.py
@@ -5,10 +5,10 @@ import pandas as pd
 # Initialize Chatham and get monthly rates
 chatham = Chatham()
 rates = chatham.get_monthly_rates()
-
+st.write(rates)
 # Convert the rates to a DataFrame if it's not already one
-if not isinstance(rates, pd.DataFrame):
-    rates = pd.DataFrame(rates)
+#if not isinstance(rates, pd.DataFrame):
+#    rates = pd.DataFrame(rates)
 
 # Plot the rates in a line chart
-st.line_chart(rates)
+#st.line_chart(rates)
