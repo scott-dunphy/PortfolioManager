d5d13b2d584262786f427463c82c50f0b2db5ec7
scott-dunphy <133469585+scott-dunphy@users.noreply.github.com>
2024-07-12 22:05:51 -0400

    Update 2_Upload.py

--- pages/2_Upload.py
+++ pages/2_Upload.py
@@ -19,6 +19,7 @@ if st.button("Upload Portfolio"):
     if properties_and_loans_file:
         properties, loans = load_properties_and_loans(properties_and_loans_file)
         noi, capex = load_cashflows(properties_and_loans_file)
+        st.write(noi)
         
         for property_obj in properties:
             property_id = property_obj.property_id
