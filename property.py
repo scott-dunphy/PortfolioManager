fcebf6a6f3f6ef2375262c53df491362bec93d08
scott-dunphy <133469585+scott-dunphy@users.noreply.github.com>
2024-07-13 10:53:13 -0400

    Update property.py

--- property.py
+++ property.py
@@ -190,6 +190,7 @@ class Property:
             cash_flows_df.at[standardized_date, 'Ownership Share'] = self.ownership_share_series.get(standardized_date, 1.0)
 
         fin_df = self.noi_capex.set_index("Date")
+        fin_df = fin_df[['Net Operating Income', 'Capital Expenditures']]
         cash_flows_df.add(fin_df,fill_value=0)
 
         if self.loan:
