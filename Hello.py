8a054fb7d97b36d4cd28e7f11735347db1337a12
scott-dunphy <133469585+scott-dunphy@users.noreply.github.com>
2024-07-14 17:35:06 -0400

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -53,7 +53,7 @@ if 'cash_flows' in st.session_state:
     cash_flows = st.session_state.cash_flows
     cash_flows = st.data_editor(cash_flows, column_config=adjusted_column_config, use_container_width=True)
     st.session_state.cash_flows = cash_flows  # Update session state with any changes made in the editor
-    for index,row in cash_flows.T.iterrows():
+    for index,row in cash_flows.iterrows():
         st.write(f"{index} : {row}")
 
     # Sum the transposed DataFrame
